4aabcb1ab8812b6fd6fa56ce0c0caf7fac74282e
Camilo Guaza <camiloguaza@gmail.com>
2023-03-07 12:13:06 -0500

    cambios que se realizaron con el menu responsive, clase 8 - fusion

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email'); //Aqui vamos  manipular el menu, por eso mandamos a llamr la clase por medio de una variable
 const desktopMenu = document.querySelector ('.desktop-menu');
+const iconmenu = document.querySelector ('.menu');
+const mobileMenu = document.querySelector ('.mobile-menu');
+
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+iconmenu.addEventListener('click', togglemobileMenu);
 
 function toggleDesktopMenu(){ //Aqui hacemos que aparezca o desapareza el elemento
     desktopMenu.classList.toggle('inactive');
 }
 
+function togglemobileMenu(){//Aqui hacemos que aparezca o desapareza el elemento
+    mobileMenu.classList.toggle('inactive')
+}
